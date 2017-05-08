ee7e2dfd564623b3ada01fc614a43509ab014f6c
Bernadeta Nowosielska <Bernadeta Nowosielska>
2017-05-08 22:23:21 +0200

    Z jakiegoś powodu działa, dodane wiązania

--- program.py
+++ program.py
@@ -12,11 +12,13 @@ class atom:
     y=0
     z=0
     ID=""
-    def __init__(__self__,a,b,c,ID):
+    element=""
+    def __init__(__self__,a,b,c,ID,element):
         __self__.x=a
         __self__.y=b
         __self__.z=c
         __self__.ID=ID
+        __self__.element=element
     def scalar_prod(__self__,p):
         s=__self__.x*p.x+__self__.y*p.y+__self__.z*p.z
         return s
@@ -35,16 +37,28 @@ class atom:
         w.z=__self__.x*p.y-__self__.y*p.x
         return w
     def __str__(__self__):
-        return ( __self__.ID.ljust(4) + str(__self__.x).rjust(8) + str(__self__.y).rjust(8) + str(__self__.z).rjust(8))   
+        return ( '%6s %2s %7.4f %7.4f %7.4f' %(__self__.ID,__self__.element, __self__.x,__self__.y,__self__.z, ))
 class monomer:
     ID=""
-    atoms= []
     number_of_atoms = 0
-    def __init__(__self__,ID,noa, atoms):
+    number_of_bonds = 0
+#    atoms=[]
+    def __init__(__self__,ID,noa, atoms, nob, bonds):
+        __self__.atoms= []
+        __self__.bonds= []
+        for i in range (nob):
+            __self__.bonds.append(bonds[i])
+#        print(len(__self__.atoms))
         __self__.ID=ID
         __self__.number_of_atoms=noa
+        __self__.number_of_bonds=nob
         for i in range (noa):
-            (__self__.atoms).append(atoms[i])
+            x=atoms[i].x
+            y=atoms[i].y 
+            z=atoms[i].z
+            ID=atoms[i].ID
+            element=atoms[i].element
+            __self__.atoms.append(atom(x,y,z,ID,element))
     def replicate(__self__):
         new_atoms=[]
         for i in range (__self__.number_of_atoms):
@@ -62,38 +76,46 @@ class monomer:
             __self__.atoms[i].z+=v.z
     def __str__(__self__):
         string="ID:  "+__self__.ID + "    Number of atoms:   " + str(__self__.number_of_atoms) + '\n'
-        for i in range(__self__.number_of_atoms):
-            string+= (__self__.atoms[i].ID.ljust(4) + str(__self__.atoms[i].x).rjust(8) + str(__self__.atoms[i].y).rjust(8) + str(__self__.atoms[i].z).rjust(8) + '\n')
+        for i in range(__self__.number_of_atoms):       
+            string+=  ( '%6s %2s %7.4f %7.4f %7.4f' %(__self__.atoms[i].ID,__self__.atoms[i].element, __self__.atoms[i].x,__self__.atoms[i].y,__self__.atoms[i].z, ))
+            string+='\n'
+        string+="Number of bonds:  " + str(__self__.number_of_bonds)+ '\n'
+        for i in range(__self__.number_of_bonds):
+            string+= ('%6s %6s %6s' %(__self__.bonds[i][0], __self__.bonds[i][1], __self__.bonds[i][2] ))+'\n'
         return string       
 ### tworzy obiekt klasy atom, następnie z atomow obiekt monomer klasy monomer, replikuje monomer do nowego obiektu
 def upload_data(table):
     atoms_param=[]  #parametry kolejnych atomow
     no_at=0  #number of atoms
     ID="?"
+    no_bonds=0
+    bonds=[]
     for w1 in range (0,len(table)):
         if table[w1][0]==table[1][1]:
             if len(table[w1])>=12 and (table[w1][12]!="?" or table[w1][13]!="?" or table [w1][14]!="?" ) and (table[w1][12]!="charge") :
                 ID=table[w1][0]     # ID of monomer
                 atom_name=table[w1][2]    #ID of atom (specific only within one monomer)
-                pierwiastek=table[w1][3]  # name of the element 
+                element=table[w1][3]  # name of the element 
                 x=float(table[w1][12])
                 y=float(table[w1][13])
                 z=float(table[w1][14])
-                atom_i = atom(x,y,z,atom_name) #obiekt klasy atom
+                atom_i = atom(x,y,z,atom_name,element) #obiekt klasy atom
                 no_at+=1
-                atoms_param.append(atom_i) #tablica zawierająca parametry atomow danego monomeru
-    monomer_i = monomer(ID, no_at, atoms_param) 
-    monomer_i.replicate()
-    return monomer_i
-#    print (monomer_i)
+                atoms_param.append(atom_i) #tablica zawierająca obiekty klasy atom
+            if len(table[w1])==7 and table[w1][6]==str(no_bonds+1):
+                bonds.append([table[w1][1], table[w1][2], table[w1][3] ])
+                no_bonds+=1
+#    print(monomer(ID, no_at, atoms_param))
+    return  monomer(ID, no_at, atoms_param, no_bonds , bonds) 
 file=open('Components-pub.cif')
 i=0
 table =[]
 number_of_lines=0
-monomers_list=[]
+monomers_list=[] 
 for line in file:  
     i+=1
 #    if i>8355: break
+#    if i>3785: break
     words=re.split("\s+",line.strip())
     table.append(words)
     number_of_lines+=1
@@ -112,6 +134,9 @@ for line in file:
     number_of_lines=0
 for l in range (len(monomers_list)):
     print (monomers_list[l])
+
+
+
         
     
     
