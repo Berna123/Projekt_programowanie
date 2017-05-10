598bfd3a59e3c8c94da61acf8208f15fa368bc02
bag22 <barbara.gruza@student.uw.edu.pl>
2017-05-10 23:07:25 +0200

    wersja robocza - drukuje atomy z grupy cooh
    ale niestety możliwe że też jakieś inne błędnie znalezione, i nie wszystkie możliwe

--- program.py
+++ program.py
@@ -13,12 +13,14 @@ class atom:
     z=0
     ID=""
     element=""
-    def __init__(__self__,a,b,c,ID,element):
+    znacznik=""
+    def __init__(__self__,a,b,c,ID,element, znacznik):
         __self__.x=a
         __self__.y=b
         __self__.z=c
         __self__.ID=ID
         __self__.element=element
+        __self__.znacznik=znacznik
     def scalar_prod(__self__,p):
         s=__self__.x*p.x+__self__.y*p.y+__self__.z*p.z
         return s
@@ -37,7 +39,7 @@ class atom:
         w.z=__self__.x*p.y-__self__.y*p.x
         return w
     def __str__(__self__):
-        return ( '%6s %2s %7.4f %7.4f %7.4f' %(__self__.ID,__self__.element, __self__.x,__self__.y,__self__.z, ))
+        return ( '%6s %2s %7.4f %7.4f %7.4f %6s' %(__self__.ID,__self__.element, __self__.x,__self__.y,__self__.z, __self__.znacznik))
 class monomer:
     ID=""
     number_of_atoms = 0
@@ -58,7 +60,8 @@ class monomer:
             z=atoms[i].z
             ID=atoms[i].ID
             element=atoms[i].element
-            __self__.atoms.append(atom(x,y,z,ID,element))
+            znacznik=atoms[i].znacznik
+            __self__.atoms.append(atom(x,y,z,ID,element,znacznik))
     def replicate(__self__):
         new_atoms=[]
         new_bonds=[]
@@ -80,44 +83,81 @@ class monomer:
     def __str__(__self__):
         string="ID:  "+__self__.ID + "    Number of atoms:   " + str(__self__.number_of_atoms) + '\n'
         for i in range(__self__.number_of_atoms):       
-            string+=  ( '%6s %2s %7.4f %7.4f %7.4f' %(__self__.atoms[i].ID,__self__.atoms[i].element, __self__.atoms[i].x,__self__.atoms[i].y,__self__.atoms[i].z, ))
+            string+=  ( '%6s %2s %7.4f %7.4f %7.4f %6s' %(__self__.atoms[i].ID,__self__.atoms[i].element, __self__.atoms[i].x,__self__.atoms[i].y,__self__.atoms[i].z, __self__.atoms[i].znacznik))
             string+='\n'
         string+="Number of bonds:  " + str(__self__.number_of_bonds)+ '\n'
         for i in range(__self__.number_of_bonds):
             string+= ('%6s %6s %6s' %(__self__.bonds[i][0], __self__.bonds[i][1], __self__.bonds[i][2] ))+'\n'
         return string       
 ### tworzy obiekt klasy atom, następnie z atomow obiekt monomer klasy monomer, replikuje monomer do nowego obiektu
-def upload_data(table):
+def upload_data(table, basia, basia2):
     atoms_param=[]  #parametry kolejnych atomow
     no_at=0  #number of atoms
     ID="?"
     no_bonds=0
     bonds=[]
+    znacznik=""
     for w1 in range (0,len(table)):
         if table[w1][0]==table[1][1]:
             if len(table[w1])>=12 and (table[w1][12]!="?" or table[w1][13]!="?" or table [w1][14]!="?" ) and (table[w1][12]!="charge") :
                 ID=table[w1][0]     # ID of monomer
-                atom_name=table[w1][2]    #ID of atom (specific only within one monomer)
+                atom_name=table[w1][1]    #ID of atom (specific only within one monomer)
                 element=table[w1][3]  # name of the element 
                 x=float(table[w1][12])
                 y=float(table[w1][13])
                 z=float(table[w1][14])
-                atom_i = atom(x,y,z,atom_name,element) #obiekt klasy atom
+                if table[w1][1]=='C': 
+                    znacznik="Ckarb"
+                    #print (table[w1])
+                else: znacznik=""
+                atom_i = atom(x,y,z,atom_name,element,znacznik) #obiekt klasy atom
                 no_at+=1
                 atoms_param.append(atom_i) #tablica zawierająca obiekty klasy atom
             if len(table[w1])==7 and table[w1][6]==str(no_bonds+1):
                 bonds.append([table[w1][1], table[w1][2], table[w1][3] ])
                 no_bonds+=1
+    for w2 in range (0, len(bonds)):
+       if ((bonds[w2][0]=="C" and ("O" in bonds[w2][1])) or (bonds[w2][1]=="C" and  ("O" in bonds[w2][0]))) and bonds[w2][2]=="SING":
+#           print (bonds[w2], ID)  # wiązanie C-Oh
+           if ("O" in bonds[w2][0]): tlen=bonds[w2][0]
+           else: tlen=bonds[w2][1] 
+           if ("C" in bonds[w2][0]): wegiel=bonds[w2][0]
+           else: wegiel=bonds[w2][1]
+           
+           basia.append(ID)
+           for w3 in range (0, len(bonds)):
+               if (("H" in bonds[w3][0]) or "H" in bonds[w3][1]) and (bonds[w3][0]==tlen or bonds[w3][1]==tlen) and (("O" in bonds[w3][0]) or ("O" in bonds[w3][1])):
+                   #print (bonds[w3], ID)
+                   if ("H" in bonds[w3][0]): wodor=bonds[w3][0]
+                   else: wodor=bonds[w3][1]
+                   
+                   basia2.append(ID)
+                   for w4 in range (0, len(atoms_param)):
+                       if atoms_param[w4].ID==wodor:
+                           print (ID, atoms_param[w4])
+                       if atoms_param[w4].ID==tlen:
+                           print (ID, atoms_param[w4])
+                       if atoms_param[w4].ID==wegiel:
+                           print (ID, atoms_param[w4])
+
+
+#           print (len(basia), len(basia2)) #basia - ilosc znalezionych wiązan raczej wegla karboksylowego z raczej tlenem 
+                                         #basia2 - ilosc O-H przylaczonych do znalezionego wczesniej wegla
+
 #    print(monomer(ID, no_at, atoms_param))
-    return  monomer(ID, no_at, atoms_param, no_bonds , bonds) 
+    return  monomer(ID, no_at, atoms_param, no_bonds , bonds)
 file=open('Components-pub.cif')
 i=0
 table =[]
 number_of_lines=0
 monomers_list={}
+spr=0
+basia=[]
+basia2=[]
 for line in file:  
     i+=1
-    if i>8355: break
+    if i>21362: break
+#    if i>8355: break
 #    if i>3785: break
     words=re.split("\s+",line.strip())
     table.append(words)
@@ -130,11 +170,18 @@ for line in file:
         if table[j][0]=="_chem_comp.type":
             if table[j][1][1:10]=="L-peptide" or table[j][1][1:10]=="L-PEPTIDE":
     # making monomer
-               monomer_i=upload_data(table)
+               monomer_i=upload_data(table, basia, basia2)
                if monomer_i.ID!="?":
                    monomers_list[monomer_i.ID]=monomer_i
+                   spr+=1
+                   
     table =[]
     number_of_lines=0
 #for l in range (len(monomers_list)):
 #    print (monomers_list[l])
 
+#for key in monomers_list:
+#    print(monomers_list[key])
+    
+    
+print (spr)
